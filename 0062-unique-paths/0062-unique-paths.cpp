c04205bad26e7d51dff3cb61fc573c0cc7472e96
Vinayak Deka <deka.vinayak@gmail.com>
2026-08-11 01:51:57 +0530

    Time: 0 ms (100%), Space: 9.4 MB (31.27%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -13,7 +13,20 @@ public:
 
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m, vector<int> (n,-1));
-        return f(m-1,n-1,dp);
-        
+        for(int i = 0; i<m; i++)
+        {
+            for(int j = 0;j<n;j++)
+            {
+                if(i==0 && j==0) dp[i][j] = 1;
+                else{
+                    int up = 0;
+                    int left = 0;
+                if(i>0) up = dp[i-1][j]; // Up side choice
+                if(j>0) left = dp[i][j-1]; // Left side choice
+                dp[i][j] = left + up; 
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
