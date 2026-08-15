7adbb37fcfb8de41a87b4645672dd35ff8045817
Vinayak Deka <deka.vinayak@gmail.com>
2026-08-15 19:58:02 +0530

    Time: 90 ms (88.68%), Space: 15.4 MB (6.91%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,28 +1,38 @@
 class Solution {
 public:
-
-    int f(int i, int j,vector<int>& nums, vector<vector<long long>> &dp)
-    {
-        if(i>j) return 0;
-        long long maxcoin = 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        for(int index = i; index<=j;index++)
-        {
-           long long coin =
-                1LL * nums[i-1] * nums[index] * nums[j+1]
-                + f(i, index-1, nums, dp)
-                + f(index+1, j, nums, dp);
-
-            maxcoin = max(maxcoin, coin);
-        }
-         return dp[i][j] = maxcoin;
-    }
     int maxCoins(vector<int>& nums) {
+
         nums.push_back(1);
-        nums.insert(nums.begin(),1);
+        nums.insert(nums.begin(), 1);
+
         int n = nums.size();
-        vector<vector<long long>> dp(n+1,vector<long long> (n+1,-1));
-        return f(1,n-2,nums,dp);  
+
+        vector<vector<long long>> dp(
+            n, vector<long long>(n, 0)
+        );
+
+        for(int i = n - 2; i >= 1; i--)
+        {
+            for(int j = i; j <= n - 2; j++)
+            {
+                long long maxcoin = 0;
+
+                for(int index = i; index <= j; index++)
+                {
+                    long long coin =
+                        1LL * nums[i-1]
+                        * nums[index]
+                        * nums[j+1]
+                        + dp[i][index-1]
+                        + dp[index+1][j];
+
+                    maxcoin = max(maxcoin, coin);
+                }
+
+                dp[i][j] = maxcoin;
+            }
+        }
+
+        return dp[1][n-2];
     }
 };
