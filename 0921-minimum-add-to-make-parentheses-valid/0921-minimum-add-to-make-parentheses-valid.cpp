322128696756ecde5a179e73f1b7919fa0a74ee4
Vinayak Deka <deka.vinayak@gmail.com>
2026-09-19 02:30:22 +0530

    Time: 0 ms (100%), Space: 8.4 MB (82.04%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+
+        int open = 0;
+        int ans = 0;
+
+        for(char c: s)
+        {
+            if(c== '(') open++;
+            else if(open>0)
+            open--;
+            else
+            ans++;
+        }
+        
+        return ans+open;
+    }
+};
